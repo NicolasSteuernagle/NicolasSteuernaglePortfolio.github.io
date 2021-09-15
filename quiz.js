75b6a10ea8db08c7b437bf77885526d89aee0c50
Nicolas Steuernagle <nicosteu@udel.edu>
2021-09-15 01:11:29 -0400

    After making sure this quiz tutorial works, I followed the tutorial to create a quiz of my own

--- quiz.js
+++ quiz.js
@@ -1,84 +1,72 @@
-// pos is position of where the user in the test or which question they're up to
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
-// this is a multidimensional array with 4 inner array elements with 5 elements inside them
-var questions = [
+var position = 0, testdiv, userProgress, question, userAnswer, possibleAnswers, A, B, C, correct = 0;
+var myQuestions = [
   {
-      question: "What is 36 + 42",
-      a: "64",
-      b: "78",
-      c: "76",
-      answer: "B"
-    },
-  {
-      question: "What is 7 x 4?",
-      a: "21",
-      b: "27",
-      c: "28",
-      answer: "C"
-    },
-  {
-      question: "What is 16 / 4?",
-      a: "4",
-      b: "6",
-      c: "3",
+      question: "What Video Game is Shown in the Image?",
+      a: "Halo",
+      b: "Destiny",
+      c: "Call of Duty",
       answer: "A"
     },
   {
-      question: "What is 8 x 12?",
-      a: "88",
-      b: "112",
-      c: "96",
+      question: "What Video Game is Shown in the Image?",
+      a: "Banjo-Kazooie",
+      b: "Super Smash Bros.",
+      c: "The Legend of Zelda",
       answer: "C"
+    },
+  {
+      question: "What Video Game is Shown in the Image?",
+      a: "Disney Universe",
+      b: "Kingdom Hearts",
+      c: "Disney Infinity",
+      answer: "B"
+    },
+  {
+      question: "What Video Game is Shown in the Image?",
+      a: "Destiny",
+      b: "Mass Effect",
+      c: "Dead Space",
+      answer: "A"
     }
-  ];
-// this get function is short for the getElementById function  
+  ]; 
 function get(x){
   return document.getElementById(x);
 }
-// this function renders a question for display on the page
 function renderQuestion(){
-  test = get("test");
-  if(pos >= questions.length){
-    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-    get("test_status").innerHTML = "Test completed";
-    // resets the variable to allow users to restart the test
-    pos = 0;
+  test = get("testdiv");
+  if(position >= myQuestions.length){
+    test.innerHTML = "<h2>You got "+correct+" of "+myQuestions.length+" questions correct</h2>";
+    get("userProgress").innerHTML = "Test completed";
+    position = 0;
     correct = 0;
-    // stops rest of renderQuestion function running when test is completed
     return false;
   }
-  get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+  get("userProgress").innerHTML = "Question "+(position+1)+" of "+myQuestions.length;
   
-  question = questions[pos].question;
-  chA = questions[pos].a;
-  chB = questions[pos].b;
-  chC = questions[pos].c;
-  // display the question
+  question = myQuestions[position].question;
+  A = myQuestions[position].a;
+  B = myQuestions[position].b;
+  C = myQuestions[position].c;
+  
   test.innerHTML = "<h3>"+question+"</h3>";
-  // display the answer options
-  // the += appends to the data we started on the line above
-  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
-  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
-  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+
+  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+A+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+B+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+C+"</label><br><br>";
   test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
 }
 function checkAnswer(){
-  // use getElementsByName because we have an array which it will loop through
-  choices = document.getElementsByName("choices");
-  for(var i=0; i<choices.length; i++){
-    if(choices[i].checked){
-      choice = choices[i].value;
+  possibleAnswers = document.getElementsByName("possibleAnswers");
+  for(var i=0; i<possibleAnswers.length; i++){
+    if(possibleAnswers[i].checked){
+      userAnswer = possibleAnswers[i].value;
     }
   }
-  // checks if answer matches the correct choice
-  if(choice == questions[pos].answer){
-    //each time there is a correct answer this value increases
+  if(userAnswer == myQuestions[position].answer){
     correct++;
   }
-  // changes position of which character user is on
-  pos++;
-  // then the renderQuestion function runs again to go to next question
+  position++;
   renderQuestion();
 }
-// Add event listener to call renderQuestion on page load event
+
 window.addEventListener("load", renderQuestion);
