16427b1fa08f6f2438a75001c66a9534ed9a2996
Nicolas Steuernagle <nicosteu@udel.edu>
2021-09-15 00:39:55 -0400

    new attemp at quiz game

--- quiz.js
+++ quiz.js
@@ -1,112 +1,108 @@
-(function(){
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
+var myQuestions = [
+    {
+      question: "What is 10/2?",
+      answers: {
+        a: '3',
+        b: '5',
+        c: '115'
+      },
+      correctAnswer: 'b'
+    },
+    {
+      question: "What is 30/3?",
+      answers: {
+        a: '3',
+        b: '5',
+        c: '10'
+      },
+      correctAnswer: 'c'
+    }
+  ];
+  
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+  
+  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+  
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+  
+    function showQuestions(questions, quizContainer){
+      // we'll need a place to store the output and the answer choices
+      var output = [];
+      var answers;
   
       // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
+      for(var i=0; i<questions.length; i++){
+        
+        // first reset the list of answers
+        answers = [];
   
-          // variable to store the list of possible answers
-          const answers = [];
+        // for each available answer...
+        for(letter in questions[i].answers){
   
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
+          // ...add an html radio button
+          answers.push(
+            '<label>'
+              + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+              + letter + ': '
+              + questions[i].answers[letter]
+            + '</label>'
           );
         }
-      );
   
-      // finally combine our output list into one string of HTML and put it on the page
+        // add this question and its answers to the output
+        output.push(
+          '<div class="question">' + questions[i].question + '</div>'
+          + '<div class="answers">' + answers.join('') + '</div>'
+        );
+      }
+  
+      // finally combine our output list into one string of html and put it on the page
       quizContainer.innerHTML = output.join('');
     }
   
-    function showResults(){
   
+    function showResults(questions, quizContainer, resultsContainer){
+      
       // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll('.answers');
-  
+      var answerContainers = quizContainer.querySelectorAll('.answers');
+      
       // keep track of user's answers
-      let numCorrect = 0;
-  
+      var userAnswer = '';
+      var numCorrect = 0;
+      
       // for each question...
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
+      for(var i=0; i<questions.length; i++){
   
         // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        
         // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
+        if(userAnswer===questions[i].correctAnswer){
           // add to the number of correct answers
           numCorrect++;
-  
+          
           // color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
+          answerContainers[i].style.color = 'lightgreen';
         }
         // if answer is wrong or blank
         else{
           // color the answers red
-          answerContainers[questionNumber].style.color = 'red';
+          answerContainers[i].style.color = 'red';
         }
-      });
+      }
   
       // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
     }
   
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-      {
-        question: "Who invented JavaScript?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "c"
-      },
-      {
-        question: "Which one of these is a JavaScript package manager?",
-        answers: {
-          a: "Node.js",
-          b: "TypeScript",
-          c: "npm"
-        },
-        correctAnswer: "c"
-      },
-      {
-        question: "Which tool can you use to ensure code quality?",
-        answers: {
-          a: "Angular",
-          b: "jQuery",
-          c: "RequireJS",
-          d: "ESLint"
-        },
-        correctAnswer: "d"
-      }
-    ];
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+      showResults(questions, quizContainer, resultsContainer);
+    }
   
-    // Kick things off
-    buildQuiz();
-  
-    // Event listeners
-    submitButton.addEventListener('click', showResults);
-  })();
+  }
