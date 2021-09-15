f71b94f4896336ba8f6ef48c3c32aa82683e066e
Nicolas Steuernagle <nicosteu@udel.edu>
2021-09-15 00:33:11 -0400

    test

--- quiz.js
+++ quiz.js
@@ -1,76 +1,112 @@
-function buildQuiz(){
-    const output = [];
-    myQuestions.forEach(
+(function(){
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
         (currentQuestion, questionNumber) => {
-        const answers = [];
-        for(letter in currentQuestion.answers){
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
             answers.push(
-                `<label>
-                    <input type = "radio" name = "questions${questionNumber}" value = "${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                    </label>`
-
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
             );
-        }
-        output.push(
-            `<div class = "question"> ${currentQuestion.question} </div>
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
             <div class="answers"> ${answers.join('')} </div>`
-        );
+          );
+        }
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
     }
-    );
-    quizContainer.innerHTML = output.join('');
-}
-function showResults(){
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-    let numCorrect = 0;
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-        const answerContainers = answerContainers[questionNumber];
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
         const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainers.querySelector(selector) || {}).value;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
         if(userAnswer === currentQuestion.correctAnswer){
-            numCorrect++;
-            answerContainers[questionNumber].style.color = 'lightgreen';
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
         }
+        // if answer is wrong or blank
         else{
-            answerContainers[questionNumber].style.color = 'red';
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
         }
-    });
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-}
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const myQuestions = [
-    {
-      question: "When Did Javascript first appear?",
-      answers: {
-        a: "July 24, 1995",
-        b: "December 4, 1995",
-        c: "August 16, 1995"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "Who owns the trademark for JavaScript?",
-      answers: {
-        a: "Microsoft",
-        b: "Apple",
-        c: "Oracle"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "JavaScript is considered a:",
-      answers: {
-        a: "Weakly Typed Language",
-        b: "Poorly Typed Language",
-        c: "Strong Typed Language",
-      },
-      correctAnswer: "a"
+      });
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
-  ];
-buildQuiz();
-
-submitButton.addEventListener('click', showResults);
-
+  
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Who invented JavaScript?",
+        answers: {
+          a: "Douglas Crockford",
+          b: "Sheryl Sandberg",
+          c: "Brendan Eich"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Which one of these is a JavaScript package manager?",
+        answers: {
+          a: "Node.js",
+          b: "TypeScript",
+          c: "npm"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+          a: "Angular",
+          b: "jQuery",
+          c: "RequireJS",
+          d: "ESLint"
+        },
+        correctAnswer: "d"
+      }
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+  })();
