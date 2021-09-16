55cbb9e6d40c1270d65214ba63008df030a0bee2
Nicolas Steuernagle <nicosteu@udel.edu>
2021-09-15 20:02:10 -0400

    Added images to quiz

--- quiz.js
+++ quiz.js
@@ -5,28 +5,32 @@ var questions = [
       a: "Halo",
       b: "Destiny",
       c: "Call of Duty",
-      answer: "A"
+      answer: "A",
+      img: "https://th.bing.com/th/id/OIP.viXe3fmD-0amhRFmLSh8WwHaEo?pid=ImgDet&rs=1"
     },
   {
       question: "What Video Game is Shown in the Image?",
       a: "Banjo-Kazooie",
       b: "Super Smash Bros.",
       c: "The Legend of Zelda",
-      answer: "C"
+      answer: "C",
+      img: "https://wallpapercave.com/wp/wp2017786.jpg"
     },
   {
       question: "What Video Game is Shown in the Image?",
       a: "Disney Universe",
       b: "Kingdom Hearts",
       c: "Disney Infinity",
-      answer: "B"
+      answer: "B",
+      img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5298bac0-b8bf-4c80-af67-725c1272dbb0/dcxqvpx-dcdbe866-e363-4efc-b836-1d3d3c86e300.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzUyOThiYWMwLWI4YmYtNGM4MC1hZjY3LTcyNWMxMjcyZGJiMFwvZGN4cXZweC1kY2RiZTg2Ni1lMzYzLTRlZmMtYjgzNi0xZDNkM2M4NmUzMDAuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.42SC_EKuJWwYM5IybRA0MVtPuAZ5Dzz2YUUf5kIdhKM"
     },
   {
       question: "What Video Game is Shown in the Image?",
       a: "Destiny",
       b: "Mass Effect",
       c: "Dead Space",
-      answer: "A"
+      answer: "A",
+      img: "https://th.bing.com/th/id/OIP.KVQXW1F6zchYqRVDGTx73QHaEK?pid=ImgDet&rs=1"
     }
   ]; 
 function get(x){
@@ -47,8 +51,11 @@ function renderQuestion(){
   chA = questions[pos].a;
   chB = questions[pos].b;
   chC = questions[pos].c;
+  img = questions[pos].img;
   
   test.innerHTML = "<h3>"+question+"</h3>";
+
+  test.innerHTML += "<img src=\"" + img + "\" width = \"200\"  height = \"200\"><br>";
 
   test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
   test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
